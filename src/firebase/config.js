bf7050d17a62a45fb73e4e922b2aa43060deaf19
Shaan Alam <shaanalam369@gmail.com>
2021-03-20 21:06:13 +0530

    login page

---
+++ src/firebase/config.js
@@ -0,0 +1,16 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDRkVRsEFfIZqVcdrMa2yfMXPGNqIL9L9Y",
+  authDomain: "instagram-f313d.firebaseapp.com",
+  projectId: "instagram-f313d",
+  storageBucket: "instagram-f313d.appspot.com",
+  messagingSenderId: "787484436145",
+  appId: "1:787484436145:web:4485cc413fdc44eef32490",
+  measurementId: "G-VRLTG5FWF7",
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+export const auth = firebaseApp.auth();
+export const storage = firebaseApp.firestore();
