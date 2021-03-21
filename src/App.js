33b37129d6afed7a185c8e68ba88523d1fcbaaf2
Shaan Alam <shaanalam369@gmail.com>
2021-03-21 10:35:29 +0530

    sigup page completed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Switch, Route } from "react-router-dom";
-import Login from "./components/Login";
 import Home from "./components/Home";
+import Login from "./components/Login";
+import Signup from './components/Signup'
 import "./assets/css/styles.css";
 
 export default function App() {
@@ -8,6 +9,7 @@ export default function App() {
     <div className="App">
       <Switch>
         <Route path="/" component={Login} exact />
+        <Route path="/signup" component={Signup} />
         <Route path="/home" component={Home} />
       </Switch>
     </div>
