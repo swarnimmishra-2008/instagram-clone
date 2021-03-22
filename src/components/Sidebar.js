450599c71a96f1ffb1143b2bcab033de4f307f95
Shaan Alam <shaanalam369@gmail.com>
2021-03-22 17:45:40 +0530

    auth process complete

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -11,8 +11,8 @@ export default function Sidebar() {
       <div className="sidebar__header">
         <Avatar alt={user?.displayName} src={user.photoURL} />
         <div className="user__info">
-          <h4 className="currentUser__username">{user?.username}</h4>
-          <p className="currentUser__fullName">{user?.displayName}</p>
+          <h4 className="currentUser__username">{user?.displayName}</h4>
+          <p className="currentUser__fullName">{user?.username}</p>
         </div>
       </div>
       <div className="suggestions">
