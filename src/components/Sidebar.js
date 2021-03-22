20391cb4593a071b1b22cc1aec1ccc96aea6bb6f
Shaan Alam <shaanalam369@gmail.com>
2021-03-22 15:14:18 +0530

    authenticaton done

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -9,13 +9,10 @@ export default function Sidebar() {
   return (
     <div className="sidebar">
       <div className="sidebar__header">
-        <Avatar
-          alt="Shaan Alam"
-          src="https://instagram.fknu1-2.fna.fbcdn.net/v/t51.2885-19/s150x150/161233805_1742142032840831_4284340814050814656_n.jpg?tp=1&_nc_ht=instagram.fknu1-2.fna.fbcdn.net&_nc_ohc=cRKDaBw0RjAAX-9bOzH&ccb=7-4&oh=dfdb2615e2580a6c3dc51e79d152f4c0&oe=607F4D7F"
-        />
+        <Avatar alt={user?.displayName} src={user.photoURL} />
         <div className="user__info">
-          <h4 className="currentUser__username">{user.username}</h4>
-          <p className="currentUser__fullName">Shaan Alam</p>
+          <h4 className="currentUser__username">{user?.username}</h4>
+          <p className="currentUser__fullName">{user?.displayName}</p>
         </div>
       </div>
       <div className="suggestions">
