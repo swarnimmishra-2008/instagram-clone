e4d9931b815fa02b6685eb182445b52c199a6b24
Shaan Alam <shaanalam369@gmail.com>
2021-03-26 16:25:37 +0530

    private routes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
-import { Switch, Route } from "react-router-dom";
+import { useEffect, useContext } from "react";
+import { Switch, Route, useHistory } from "react-router-dom";
 import Home from "./components/Home";
 import Login from "./components/Login";
-import Signup from './components/Signup'
+import Signup from "./components/Signup";
 import "./assets/css/styles.css";
+import { PrivateRoute } from "./PrivateRoute";
+import { Context } from "./Context/GlobalState";
+import { db, auth } from "./firebase/config";
 
 export default function App() {
+  const { user, setUser } = useContext(Context);
+  const history = useHistory();
+
   return (
     <div className="App">
       <Switch>
         <Route path="/" component={Login} exact />
         <Route path="/signup" component={Signup} />
-        <Route path="/home" component={Home} />
+        <PrivateRoute path="/home" component={Home} />
       </Switch>
     </div>
   );
