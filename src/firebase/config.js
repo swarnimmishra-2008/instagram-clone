51faeb449d0b92f3d65369699e794b9022d0a350
Shaan Alam <shaanalam369@gmail.com>
2021-04-03 19:35:21 +0530

    moved firebase configuration to .env files

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -2,14 +2,15 @@ import firebase from "firebase";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAzE0n7_mljIlmvwgWlYXiJQ4SPAApZE_c",
-  authDomain: "instagram-c68d0.firebaseapp.com",
-  projectId: "instagram-c68d0",
-  storageBucket: "instagram-c68d0.appspot.com",
-  messagingSenderId: "558238019752",
-  appId: "1:558238019752:web:942ea41e14c8a860e2129d",
-  measurementId: "G-V5Q8FEMMRS"
+  apiKey: REACT_APP_API_KEY,
+  authDomain: REACT_APP_AUTH_DOMAIN,
+  projectId: REACT_APP_PROJECT_ID,
+  storageBucket: REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: REACT_APP_MESSAGING_SENDER_ID,
+  appId: REACT_APP_APP_ID,
+  measurementId: REACT_APP_MEASUREMENT_ID
 };
+
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 
 export const auth = firebaseApp.auth();
