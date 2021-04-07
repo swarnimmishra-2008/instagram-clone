9aaf3152ff67c0b8d069f15a38761c998392f9cb
Shaan Alam <shaanalam369@gmail.com>
2021-04-07 18:02:24 +0530

    modified auth pages

--- src/components/Login.js
+++ src/components/Login.js
@@ -47,12 +47,9 @@ export default function Login({ history }) {
 
   return (
     <div className="login__container">
-      <div className="hero__bg">
-        <img src="/assets/images/insta_hero_bg.png" alt="Hero bg" />
-      </div>
+      <div className="hero__bg"></div>
       <div className="form__area">
         <div className="form">
-          <img src="/assets/images/insta_logo.png" alt="Instagram Logo" />
           <form onSubmit={handlelogin}>
             <div className="form__field">
               <input
@@ -109,20 +106,6 @@ export default function Login({ history }) {
           </p>
         </div>
         <p className="gettheapp">Get the App</p>
-        <div className="stores">
-          <a href="https://apps.apple.com/app/instagram/id389801252?vt=lo">
-            <img
-              src="https://www.instagram.com/static/images/appstore-install-badges/badge_ios_english-en.png/180ae7a0bcf7.png"
-              alt="App Store"
-            />
-          </a>
-          <a href="https://play.google.com/store/apps/details?id=com.instagram.android&referrer=utm_source%3Dinstagramweb%26utm_campaign%3DloginPage%26ig_mid%3D6A647A9F-A56E-4159-9B48-90D97AD8E20D%26utm_content%3Dlo%26utm_medium%3Dbadge">
-            <img
-              src="https://www.instagram.com/static/images/appstore-install-badges/badge_android_english-en.png/e9cd846dc748.png"
-              alt="Play Store"
-            />
-          </a>
-        </div>
       </div>
     </div>
   );
