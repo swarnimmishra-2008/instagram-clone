6a73011c0fe45d88a5820ad794a9fd8ed443d25d
swarnimmishra-2008 <79363779+swarnimmishra-2008@users.noreply.github.com>
2022-03-04 13:08:40 +0530

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -2,13 +2,12 @@ import firebase from "firebase";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
-  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
+  apiKey: "AIzaSyAz6F21ivJZWi_XHVijdVC_bX6fkcExVLY",
+  authDomain: "instagram-3dd93.firebaseapp.com",
+  projectId: "instagram-3dd93",
+  storageBucket: "instagram-3dd93.appspot.com",
+  messagingSenderId: "425432887210",
+  appId: "1:425432887210:web:eb48fcf5cd1f27bcc7c5d3"
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
